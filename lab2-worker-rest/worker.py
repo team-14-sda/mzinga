75cd407240f6610e91a3a2722168b0940cde0ae5
simonetagliaferri <simone.tagliaferri@outlook.com>
2026-05-13 17:26:18 +0200

    Lab2 - Refactored worker for step A.

--- lab2-worker-rest/worker.py
+++ lab2-worker-rest/worker.py
@@ -7,25 +7,32 @@ import importlib
 lab1_worker = importlib.import_module("lab1-worker.worker")
 slate_to_html, send_email = lab1_worker.slate_to_html, lab1_worker.send_email
 
-def run_worker():
-    load_dotenv()
-    poll_time = int(os.getenv("POLL_INTERVAL_SECONDS"))
-    email = os.getenv("MZINGA_EMAIL", "s361334@studenti.polito.it")
-    password = os.getenv("MZINGA_PASSWORD", "aaa")
-    url = os.getenv("MZINGA_URL", "http://localhost:3000")
+load_dotenv()
+poll_time = int(os.getenv("POLL_INTERVAL_SECONDS"))
+email = os.getenv("MZINGA_EMAIL", "s361334@studenti.polito.it")
+password = os.getenv("MZINGA_PASSWORD", "aaa")
+url = os.getenv("MZINGA_URL", "http://localhost:3000")
+
+def login():
     post_url = url + "/api/users/login"
     data = {"email": email, "password": password}
     post_response = requests.post(url=post_url, json=data)
-    token = post_response.json()["token"]
+    return post_response.json()["token"]
+
+def authorization_header(token):
+    return {"Authorization": "Bearer " + token}
+
+def run_worker():
+    token = login()
+    header = authorization_header(token)
     get_url = url + "/api/communications?where[status][equals]=pending&depth=1"
-    patch1_url = url + "/api/communications/"
-    header = {"Authorization": "Bearer " + token}
+    patch_processing_url = url + "/api/communications/"
+
     while True:
         get_response = requests.get(url=get_url, headers=header)
         if get_response.status_code == 401:
-            post_response = requests.post(url=post_url, json=data)
-            token = post_response.json()["token"]
-            header = {"Authorization": "Bearer " + token}
+            token = login()
+            header = authorization_header(token)
             continue
         docs = get_response.json()["docs"]
         if not docs:
@@ -34,13 +41,11 @@ def run_worker():
             for doc in docs:
                 doc_id = doc["id"]
                 status = {"status": "processing"}
-                patch1_response = requests.patch(url=patch1_url+doc_id, json=status, headers=header)
+                patch_processing_response = requests.patch(url=patch_processing_url+doc_id, json=status, headers=header)
                 print("Processing started")
-                if patch1_response.status_code == 401:
-                    post_response = requests.post(url=post_url, json=data)
-                    token = post_response.json()["token"]
-                    header = {"Authorization": "Bearer " + token}
-                    print("Taking new token")
+                if patch_processing_response.status_code == 401:
+                    token = login()
+                    header = authorization_header(token)
                     continue
                 to_emails = list()
                 cc_emails = list()
@@ -56,18 +61,16 @@ def run_worker():
                     html_content = slate_to_html(doc.get("body"))
                     send_email(to_emails, cc_emails, bcc_emails, doc, html_content)
                     status = {"status": "sent"}
-                    patch2_response = requests.patch(url=patch1_url+doc_id, json=status, headers=header)
+                    patch_sent_response = requests.patch(url=patch_processing_url+doc_id, json=status, headers=header)
                     print("Sending email")
-                    if patch2_response.status_code == 401:
-                        post_response = requests.post(url=post_url, json=data)
-                        token = post_response.json()["token"]
-                        header = {"Authorization": "Bearer " + token}
-                        print("Taking new token")
+                    if patch_sent_response.status_code == 401:
+                        token = login()
+                        header = authorization_header(token)
                         continue
                 except Exception as e:
                     print("Exception " + str(e))
                     status = {"status": "failed", "error": str(e)}
                     try:
-                        requests.patch(url=patch1_url + doc_id, json=status, headers=header)
+                        requests.patch(url=patch_processing_url + doc_id, json=status, headers=header)
                     except Exception as e:
                         print("Critical: could not update status to failed." + str(e))
